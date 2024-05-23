87bfb0e23aa4a55e6e909861125bbf70860e0926
unknown <trantrungkienl2703@gmail.com>
2024-05-23 11:36:13 +0700

    add normalize api response in deleted method

--- src/main/java/com/project/kien/identity_service/controller/UserController.java
+++ src/main/java/com/project/kien/identity_service/controller/UserController.java
@@ -40,9 +40,13 @@ public class UserController {
     }
 
     @DeleteMapping("{userId}")
-    String deleteUser(@PathVariable String userId) {
+    ApiResponse<String> deleteUser(@PathVariable String userId) {
+        ApiResponse apiResponse = new ApiResponse();
+        User user =userService.getUser(userId);
+        apiResponse.setCode(8888);
+        apiResponse.setMessage("user with username"+user.getUsername()+" has been deleted");
         userService.deleteUser(userId);
-        return "user has been deleted";
+        return apiResponse;
     }
 }
 
