776e30342f0cd260551e1f9191bc053302e6e142
kien <trantrungkienl2703@gmail.com>
2024-06-10 21:29:44 +0700

    advanced handle exception

--- src/main/java/com/project/kien/identity_service/configuration/SecurityConfig.java
+++ src/main/java/com/project/kien/identity_service/configuration/SecurityConfig.java
@@ -40,7 +40,11 @@ public class SecurityConfig {
                 request.requestMatchers(HttpMethod.POST,PUBLIC_ENDPOINTS).permitAll()
         .anyRequest().authenticated());
         httpSecurity.oauth2ResourceServer(oauth2 ->
-                oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder()).jwtAuthenticationConverter(jwtAuthenticationConverter())));
+                oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder()).jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                        .authenticationEntryPoint(new JwtAuthenticationEntryPoint())
+        );
+
+
         HttpSecurity csrf = httpSecurity.csrf(AbstractHttpConfigurer::disable);
 
         return httpSecurity.build();
--- src/main/java/com/project/kien/identity_service/exception/ErrorCode.java
+++ src/main/java/com/project/kien/identity_service/exception/ErrorCode.java
@@ -1,30 +1,30 @@
 package com.project.kien.identity_service.exception;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+@Getter
 public enum ErrorCode {
-    INVALID_YEAR(1007,"invalid year"),
-    INVALID_KEY(1005,"invalid message key"),
-    USER_EXISTED(1002,"User existed"),
-    USER_NOT_EXISTED(1009,"User not existed"),
-    USER_NOT_FOUND(1003,"User not found"),
-    UNCATEGORIZED(9999,"uncategorized error"),
-    USERNAME_VALID(1004,"Username must be at least 4 character"),
-    PASSWORD_VALID(1006,"Password must be at least 4 character"),
-    UNAUTHENTICATED(1001,"unauthenticated")
-
+    INVALID_YEAR(1007,"invalid year",HttpStatus.BAD_REQUEST),
+    INVALID_KEY(1005,"invalid message key",HttpStatus.BAD_REQUEST),
+    USER_EXISTED(1002,"User existed",HttpStatus.BAD_REQUEST),
+    USER_NOT_EXISTED(1009,"User not existed",HttpStatus.NOT_FOUND),
+    USER_NOT_FOUND(1003,"User not found",HttpStatus.BAD_REQUEST),
+    UNCATEGORIZED(9999,"uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR),
+    USERNAME_VALID(1004,"Username must be at least 4 character",HttpStatus.BAD_REQUEST),
+    PASSWORD_VALID(1006,"Password must be at least 4 character",HttpStatus.BAD_REQUEST),
+    UNAUTHENTICATED(1001,"unauthenticated",HttpStatus.UNAUTHORIZED),
+    UNAUTHOZIED(1006,"you don't have permission",HttpStatus.FORBIDDEN)
     ;
     private int code;
     private String message;
+    private HttpStatusCode statusCode;
 
-    public int getCode() {
-        return code;
-    }
 
-    public String getMessage() {
-        return message;
-    }
-
-    ErrorCode(int code, String message) {
+    ErrorCode(int code, String message,HttpStatusCode statusCode) {
         this.code = code;
         this.message = message;
+        this.statusCode =statusCode;
+
     }
 }
